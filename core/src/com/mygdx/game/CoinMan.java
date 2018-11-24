26f2cae35ef8f5328a577b66d97b0a6f989b1564
areeshaFatima <areeshaf7@gmail.com>
2018-11-25 01:12:55 +0530

    man move

--- core/src/com/mygdx/game/CoinMan.java
+++ core/src/com/mygdx/game/CoinMan.java
@@ -9,11 +9,19 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
+	int pause=0;
+	Texture[] man;
+	int manState=0;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background=new Texture("bg.png");
+		man=new Texture[4];
+		man[0]=new Texture("frame-1.png");
+		man[1]=new Texture("frame-2.png");
+        man[2]=new Texture("frame-3.png");
+        man[3]=new Texture("frame-4.png");
 
 	}
 
@@ -22,6 +30,18 @@ public class CoinMan extends ApplicationAdapter {
 
 		batch.begin();
 		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        if(pause<8){
+            pause++;
+        }else {
+        	pause=0;
+            if(manState<3){
+                manState++;
+            }else {
+                manState=0;
+            }
+        }
+        batch.draw(man[manState],Gdx.graphics.getWidth()/2-man[manState].getWidth()/2,Gdx.graphics.getHeight()/2);
+
 		batch.end();
 	}
 	
